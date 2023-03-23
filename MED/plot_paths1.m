39c6551bfc4b7315d00186fc7ca91034beb6992c
xtalia00 <xtalia00@vutbr.cz>
2023-03-23 13:55:14 +0100

    new plotting aproach

--- MED/plot_paths1.m
+++ MED/plot_paths1.m
@@ -1,10 +1,17 @@
-function [] = plot_paths1(image, ant_paths)
+function [] = plot_paths1(ant_paths)
 %UNTITLED11 Summary of this function goes here
 %   Detailed explanation goes here
-figure
-imshow(image)
-hold on
-bar = ["r-","b-","g-","m-","c-","y-"];
-for i = 1:length(ant_paths)
-    plot(ant_paths{i}(:,1), ant_paths{i}(:,2), bar(i))
+pathToAnts = 'V:\4_mag\Lecture6_23\MED\Ants';
+fileList = dir(fullfile(pathToAnts, '*.jpg'));
+    bar = ["r","b","g","m","c","y"];
+for i = 1:size(fileList,1)
+    image = strcat(fileList(i).folder,'\',fileList(i).name);
+    figure (1)
+    imshow(image)
+    hold on
+    for j=1:6
+    line(ant_paths{j}(1:i,1), ant_paths{j}(1:i,2),'Color',bar(j))
+    end
+    hold off
+    
 end
